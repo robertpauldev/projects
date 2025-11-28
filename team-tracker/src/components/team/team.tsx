22c384651888b1b5065b823b4ccac1f4849a98be
Robert Paul <rpauldesign@gmail.com>
2025-11-28 20:32:09 +0000

    feat: mocking team member template

--- team-tracker/src/components/team/team.tsx
+++ team-tracker/src/components/team/team.tsx
@@ -1,10 +1,30 @@
+// Data
+import { type Member, createMember } from "../../data/members";
+
+// Styles
 import styles from "./team.module.scss";
+
+const theMembers: Member[] = [];
+
+for ( let i = 0; i < 10; i++ ) {
+  theMembers.push( createMember() );
+}
 
 const Team = () => {
   return (
     <div className={ styles["team-wrap"] }>
       <h2 className={ styles["team__title"] }>Meet the team</h2>
-      <ul className={ styles["team__members"] } />
+      <ul className={ styles["team__members"] }>
+        {
+          theMembers.map( ( data ) => (
+            <li>
+              <img src={ data.avatar } />
+              <h3>{ `${ data.firstName } ${ data.lastName }` }</h3>
+              <h4>{ data.role }</h4>
+            </li>
+           ) )
+        }
+      </ul>
     </div>
   );
 }
