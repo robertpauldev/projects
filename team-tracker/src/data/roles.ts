cde270119fbaefd1f966a519b6df52edfbdb7288
Robert Paul <rpauldesign@gmail.com>
2025-11-28 22:11:10 +0000

    chore: separating roles

---
+++ team-tracker/src/data/roles.ts
@@ -0,0 +1,9 @@
+const roles: Array<string> = [
+  "Junior Software Engineer",
+  "Software Engineer",
+  "Senior Software Engineer",
+  "Principal Software Engineer",
+  "Staff Engineer"
+] as const;
+
+export default roles;
