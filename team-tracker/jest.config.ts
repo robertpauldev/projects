94c490aba8702e8a15376daaa15768d3c9167e00
Robert Paul <rpauldesign@gmail.com>
2025-11-28 22:53:41 +0000

    chore: Jest setup

---
+++ team-tracker/jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest/presets/js-with-ts',
+  testEnvironment: 'jsdom',
+  testMatch: ['**/__tests__/**/*.ts', '**/__tests__/**/*.tsx'],
+  extensionsToTreatAsEsm: ['.ts'],
+  globals: {
+    'ts-jest': {
+      useESM: true,
+    },
+  },
+};
+
+export default config;
