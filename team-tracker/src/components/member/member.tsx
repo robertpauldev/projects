37de364548795242cc17b364f26f368933805644
Robert Paul <rpauldesign@gmail.com>
2025-11-28 21:45:11 +0000

    feat: first pass at `<Member />` component

---
+++ team-tracker/src/components/member/member.tsx
@@ -0,0 +1,23 @@
+// Components/member/Member.tsx
+import React from "react";
+import { type MemberType } from "../../data/members";
+import styles from "./member.module.scss";
+
+interface MemberProps {
+  member: MemberType;
+}
+
+const Member: React.FC<MemberProps> = ({ member }) => {
+
+  const fullName = `${ member.firstName } ${ member.lastName }`;
+
+  return (
+    <li className={styles.member}>
+      <img className={ styles["member__avatar"] } src={ member.avatar } alt={ fullName } />
+      <h3 className={ styles["member__name"] }>{ fullName }</h3>
+      <h4 className={ styles["member__role"] }>{ member.role }</h4>
+    </li>
+  );
+};
+
+export default Member;
