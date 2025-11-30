441db5eb55a1a5f2d230ef5e342c85009bbf49f8
Robert Paul <rpauldesign@gmail.com>
2025-11-30 11:35:01 +0000

    feat: first pass `<Data />` comp

--- team-tracker/src/App.tsx
+++ team-tracker/src/App.tsx
@@ -1,10 +1,12 @@
 import Team from "./components/team/team";
+import Data from "./components/data/data";
 import styles from "./App.module.scss";
 
 function App() {
   return (
     <>
       <h1 className={ styles.heading }>Team Tracker</h1>
+      <Data />
       <Team />
     </>
   )
