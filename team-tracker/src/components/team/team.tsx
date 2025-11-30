d50d0a237d28bb0e67e0990e4013683eb54007ef
Robert Paul <rpauldesign@gmail.com>
2025-11-30 22:08:18 +0000

    feat: using Session Storage to cache Faker.js data

--- team-tracker/src/components/team/team.tsx
+++ team-tracker/src/components/team/team.tsx
@@ -1,16 +1,32 @@
-import React from "react";
+import { useState } from "react";
 import { type MemberType, createMember } from "../../data/members";
 import Member from "../member/member";
 import styles from "./team.module.scss";
 
-const theMembers: MemberType[] = Array.from({ length: 30 }, () => createMember());
+const getMembers = () => {
+
+  const cachedMembers = sessionStorage.getItem( "cached-members" );
+
+  if ( cachedMembers ) {
+    return JSON.parse( cachedMembers );
+  }
+
+  const theMembers: MemberType[] = Array.from({ length: 30 }, () => createMember());
+
+  sessionStorage.setItem( "cached-members", JSON.stringify( theMembers ) );
+
+  return theMembers;
+}
 
 const Team: React.FC = () => {
+
+  const [ members ] = useState( () => getMembers() );
+
   return (
     <div className={styles["team"]}>
       <h2 className={styles["team__title"]}>Meet the team</h2>
       <ul className={styles["team__members"]}>
-        {theMembers.map((member) => (
+        {members.map((member: MemberType) => (
           <Member key={member._id} member={member} />
         ))}
       </ul>
