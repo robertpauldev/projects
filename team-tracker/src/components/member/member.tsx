c044a8fc2e13aa8fc1482dbfb05032bf22b39991
Robert Paul <rpauldesign@gmail.com>
2025-11-30 21:31:59 +0000

    feat: `Member` improvements

--- team-tracker/src/components/member/member.tsx
+++ team-tracker/src/components/member/member.tsx
@@ -1,4 +1,3 @@
-// Components/member/Member.tsx
 import React from "react";
 import { type MemberType } from "../../data/members";
 import styles from "./member.module.scss";
@@ -17,14 +16,14 @@ const Member: React.FC<MemberProps> = ({ member }) => {
       className={ `member ${ styles.member }` }
       data-show={ `true` } 
       data-role={ slugifyString( member.role ) }
-      data-location={ slugifyString( member.location ) }
     >
       <img className={ styles["member__avatar"] } src={ member.avatar } alt={ fullName } />
       <h3 className={ styles["member__name"] }>{ fullName }</h3>
       <h4 className={ styles["member__role"] }>{ member.role }</h4>
-      <ul>
-        <li>{ member.location }</li>
-        <li>{ member.gitHub }</li>
+      <ul className={ styles["member__meta"] }>
+        <li className={ styles["member__meta-github"] }>
+          <span>{ member.gitHub }</span>
+        </li>
       </ul>
     </li>
   );
