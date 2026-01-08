940be122e33c029528f906d5220cfbd984142be1
Robert Paul <rpauldesign@gmail.com>
2026-01-08 21:29:05 +0000

    chore: adding function to handle date suffix

--- bdgt/src/components/table/table.tsx
+++ bdgt/src/components/table/table.tsx
@@ -6,6 +6,19 @@ type Props = {
   onRemove: (id: string) => void;
 };
 
+const ordinalSuffixDate = ( date: number ) => {
+  switch ( date ) {
+    case 1:
+      return "1st";
+    case 2:
+      return "2nd";
+    case 3:
+      return "3rd";
+    default:
+      return `${ date }th`;
+  }
+}
+
 const Table = ( { tableData, totalValue, onRemove }: Props ) => {
 
   if ( totalValue === 0 ) {
