f6f647b514f64e458ece0486ff169355f0b1ee17
Robert Paul <rpauldesign@gmail.com>
2026-01-09 21:06:47 +0000

    chore: adding cost types, simplifying date selection

--- bdgt/src/components/form/form.tsx
+++ bdgt/src/components/form/form.tsx
@@ -1,4 +1,4 @@
-import type { TableEntry } from "../../utils/types";
+import { costTypes, type CostType, type TableEntry } from "../../utils/types";
 import styles from "./form.module.scss";
 
 type Props = {
@@ -14,16 +14,27 @@ const Form = ( { formType, setFormData }: Props ) => {
 
     const form = e.currentTarget;
     const inputs = form.querySelectorAll( "input" );
+    const select = form.querySelector( "select" );
 
     const title = inputs[0].value.trim();
-    const sum = parseFloat( inputs[1].value );
-    const date = parseInt( inputs[2].value, 10 );
+    const type: CostType = select?.value;
+
+    const sum   = parseFloat( inputs[1].value );
+    const date  = parseInt( inputs[2].value.slice(-2), 10 );
+    console.log(date);
+    // const date  = parseInt( inputs[2].value, 10 );
 
     if ( !title || isNaN( sum) || isNaN(date) ) {
       return;
     }
 
-    const newEntry: TableEntry = { id: crypto.randomUUID(), title, sum, date };
+    const newEntry: TableEntry = {
+      id: crypto.randomUUID(),
+      type,
+      title,
+      sum,
+      date
+    };
 
     setFormData( prev => [ ...prev, newEntry ] );
 
@@ -41,23 +52,31 @@ const Form = ( { formType, setFormData }: Props ) => {
           <input
             className={ styles["form__input"] }
             type="text"
-            placeholder={ `Add ${ formType } title` }
+            placeholder="Title"
           />
+
+          { formType === "cost" ?
+            <select className={ styles["form__select"] }>
+              {
+                costTypes.map(
+                  ( i, index ) => <option key={ index } value={ i }>{ i }</option>
+                )
+              }
+            </select> : ""
+          }
 
           <input
             className={ styles["form__input"] }
             type="number"
             min="0"
             step="0.01"
-            placeholder={ `Add ${ formType } amount per month` }
+            placeholder={ formType }
           />
 
           <input
             className={ styles["form__input"] }
-            type="number"
-            min="1"
-            max="31"
-            placeholder={ `Add ${ formType } date` }
+            type="date"
+            placeholder="Date"
           />
 
           <button
