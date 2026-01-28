12410dd857fa09e1700579da348e01d83896fc47
Robert Paul <rpauldesign@gmail.com>
2026-01-28 20:39:03 +0000

    feat: adding `ordinalSuffixDate` unit test

---
+++ bdgt/tests/utils.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from "vitest"
+import { ordinalSuffixDate } from "../src/utils/utils"
+
+describe( "ordinalSuffixDate", () => {
+  it.each(
+    [
+      { date: 1, expected: "1st" },
+      { date: 2, expected: "2nd" },
+      { date: 3, expected: "3rd" },
+      { date: 4, expected: "4th" },
+      { date: 21, expected: "21st" },
+      { date: 22, expected: "22nd" },
+      { date: 23, expected: "23rd" },
+      { date: 31, expected: "31st" },
+    ]
+  )( `returns $expected from $date`, ( { date, expected } ) => {
+    expect( ordinalSuffixDate( date ) ).toStrictEqual( expected );
+  } );
+} );
